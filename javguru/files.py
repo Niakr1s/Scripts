be7570a1914398a5c091e9262456479da3c5c30e
Niakr1s <pavel2188@gmail.com>
2025-11-29 15:18:41 +0300

    fixed regex once more

--- javguru/files.py
+++ javguru/files.py
@@ -1,6 +1,6 @@
 import re
 
-RE = re.compile(r"^\[(.+)\]\W*(.*?)\W*(\.mp4)?$")
+RE = re.compile(r"^\[(.+?)\]\s*(.*?)\W*(\.mp4)?$")
 
 
 def extract_id_and_description(filename: str) -> tuple[str, str]:
@@ -19,4 +19,10 @@ if __name__ == "__main__":
     )
     assert ("SOMEID-12345", "Some description") == extract_id_and_description(
         "[SOMEID-12345]Some description.mp4"
+    )
+    assert (
+        "JUQ-111",
+        "The second J-cup exclusive! She appears in a popular NTR work! Town Camping NTR – Wife’s [Warning] Cuckold Footage of tent creampie – Kana Kusakabe",
+    ) == extract_id_and_description(
+        "[JUQ-111] The second J-cup exclusive! She appears in a popular NTR work! Town Camping NTR – Wife’s [Warning] Cuckold Footage of tent creampie – Kana Kusakabe.mp4"
     )
